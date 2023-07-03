deb79a584050d634fc882f2316f5618def5d3bf6
loltyler1dotcom-discount-code-alpha <danielyangkang@gmail.com>
2023-07-02 20:26:00 -0700

    week 1 final: file handles have no transparency, assign task in later weeks

--- client/client.py
+++ client/client.py
@@ -1,6 +1,9 @@
 import socket
 import struct
 
+host = '127.0.0.1'  # Server IP address
+port = 1234 # Server port
+
 def create_request(filename):
     operation_type = b'\x01'
     filename_length = struct.pack('!B', len(filename))
@@ -12,7 +15,7 @@ def create_request(filename):
 def read_request(file_handle, offset, num_bytes):
     operation_type = b'\x02'
     file_handle_length = struct.pack('!B', len(file_handle))
-    file_handle_bytes = file_handle.encode('ascii')
+    file_handle_bytes = file_handle
     offset_bytes = struct.pack('!Q', offset)
     num_bytes_bytes = struct.pack('!I', num_bytes)
 
@@ -22,7 +25,7 @@ def read_request(file_handle, offset, num_bytes):
 def write_request(file_handle, offset, data):
     operation_type = b'\x03'
     file_handle_length = struct.pack('!B', len(file_handle))
-    file_handle_bytes = file_handle.encode('ascii')
+    file_handle_bytes = file_handle
     offset_bytes = struct.pack('!Q', offset)
     data_length = struct.pack('!B', len(data))
     data_bytes = data.encode('ascii')
@@ -30,17 +33,15 @@ def write_request(file_handle, offset, data):
     request = operation_type + file_handle_length + file_handle_bytes + offset_bytes + data_length + data_bytes
     return request
 
-def remove_request(filename):
+def remove_request(file_handle):
     operation_type = b'\x04'
-    filename_length = struct.pack('!B', len(filename))
-    filename_bytes = filename.encode('ascii')
+    filename_length = struct.pack('!B', len(file_handle))
+    filename_bytes = file_handle
 
     request = operation_type + filename_length + filename_bytes
     return request
 
 def send_request(request):
-    host = '127.0.0.1'  # Server IP address
-    port = 5432 # Server port
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect((host, port))
@@ -54,7 +55,9 @@ def send_request(request):
 
 # Example usage:
 if __name__ == "__main__":
-    # socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("127.0.0.1", 5432))
+    #notes: file handles should be in bytes
     create_request_data = create_request('example.txt')
-    response = send_request(create_request_data)
+    fh = b'/home/daniel/cloud-dfs/example.txt'
+    request_data = remove_request(fh)
+    response = send_request(request_data)
     print(response)
--- server/server.py
+++ server/server.py
@@ -1,12 +1,20 @@
 import os
 import socket
 import struct
+import enum
+
+class RequestType(enum.Enum):
+    CREATE = 0x01
+    READ = 0x02
+    WRITE = 0x03
+    REMOVE = 0X04
 
 class DFSServer:
     def __init__(self, host, port):
         self.host = host
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     def start(self):
         self.socket.bind((self.host, self.port))
@@ -22,19 +30,19 @@ class DFSServer:
         data = conn.recv(1024)
         print(f"Received data: {data}")
 
-        if data[0] == 0x01:  # CREATE request
+        if data[0] == RequestType.CREATE.value:  # CREATE request
             # Extract filename from request
             filename_len = data[1]
             filename = data[2:2+filename_len].decode()
 
             # Create file and send file handle back to client
-            with open(filename, 'w') as f:
-                pass
+            file = open(filename, 'w')
+            file.close()
             fh = os.path.abspath(filename)
-            response = struct.pack("!IB", 0x01, len(fh)) + fh.encode()
+            response = struct.pack("!1BI", 0x01, len(fh)) + fh.encode()
             conn.sendall(response)
 
-        elif data[0] == 0x02:  # READ request
+        elif data[0] == RequestType.READ.value:  # READ request
             # Extract file handle, offset, and count from request
             fh_len = data[1]
             fh = data[2:2+fh_len].decode()
@@ -45,13 +53,14 @@ class DFSServer:
             with open(fh, 'r') as f:
                 f.seek(offset)
                 data = f.read(count)
-            response = struct.pack("!IB", 0x02, len(data)) + data.encode()
+            response = struct.pack("!BI", 0x02, len(data)) + data.encode()
             conn.sendall(response)
 
-        elif data[0] == 0x03:  # WRITE request
+        elif data[0] == RequestType.WRITE.value:  # WRITE request
             # Extract file handle, offset, and data from request
             fh_len = data[1]
-            fh = data[2:2+fh_len].decode()
+            fh = data[2:2+fh_len].decode('utf-8')
+            print(fh)
             offset = struct.unpack("!Q", data[2+fh_len:2+fh_len+8])[0]
             data_len = data[2+fh_len+8]
             data = data[2+fh_len+9:2+fh_len+9+data_len].decode()
@@ -60,17 +69,17 @@ class DFSServer:
             with open(fh, 'r+') as f:
                 f.seek(offset)
                 f.write(data)
-            response = struct.pack("!IB", 0x03, 0)
+            response = struct.pack("!BI", 0x03, 0)
             conn.sendall(response)
 
-        elif data[0] == 0x04:  # REMOVE request
+        elif data[0] == RequestType.REMOVE.value:  # REMOVE request
             # Extract filename from request
             filename_len = data[1]
             filename = data[2:2+filename_len].decode()
 
             # Remove file and send status code back to client
             os.remove(filename)
-            response = struct.pack("!IB", 0x04, 0)
+            response = struct.pack("!BI", 0x04, 0)
             conn.sendall(response)
 
         conn.close()
